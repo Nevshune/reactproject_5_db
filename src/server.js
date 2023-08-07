910d4420b6c1a7054a26dd5532bf21b6ba7a2075
unknown <skawldhks@naver.com>
2023-08-07 13:22:54 +0900

    230807 third

--- src/server.js
+++ src/server.js
@@ -10,7 +10,10 @@ const PORT = 8080;
 const app = express();
 
 const corsOptions = {
-    origin: ["http://localhost:3000"],
+    origin: [
+        "http://localhost:3000",
+        "https://web-react-class-5th-20230703-eu1k2lll07brk9.sel4.cloudtype.app",
+    ],
     methods: ["GET", "POST"],
     credentials: true,
 };
